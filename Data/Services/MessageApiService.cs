d5da59a69a1c455d50ee1675ed720b1c2f4ab701
Avisalon <bamboocho007@gmail.com>
2023-06-02 11:45:40 +0300

    added chat component

--- Data/Services/MessageApiService.cs
+++ Data/Services/MessageApiService.cs
@@ -1,3 +1,6 @@
+using myChat.Enums;
+using WsBlazor.Data.DTO;
+
 namespace WsBlazor.Data.Services
 {
   public class MessageApiService
@@ -6,6 +9,12 @@ namespace WsBlazor.Data.Services
     public MessageApiService(HttpClient _httpClient)
     {
       httpClient = _httpClient;
+    }
+
+    public async Task<List<Message>?> GetMessages(Guid chatId)
+    {
+      var data = DateTime.UtcNow;
+      return await httpClient.GetFromJsonAsync<List<Message>>($"http://localhost:5149/Messages/{chatId}?Limit=10&Direction={MessageDirecton.NEXT}&From={data.ToString("o")}");
     }
   }
 }
